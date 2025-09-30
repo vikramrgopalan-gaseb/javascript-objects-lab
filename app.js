b8450f4af8cc52ac987a37055918b7e0348a4f23
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-09-30 10:25:15 -0600

    9/30

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('Hello world')
+/* console.log('Hello world') */
+
+const pokemon = require('./data.js')
+
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+
+/* console.dir(pokemon, { maxArrayLength: null }) */
+
+/* console.log(pokemon.name); */
+
+/* console.log(game) */
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+game.difficulty = ['Easy', 'Med', 'Hard']
+
+console.log(game.difficulty)
+
+game.difficulty = 'Med'
+
+console.log(game.difficulty)
+
